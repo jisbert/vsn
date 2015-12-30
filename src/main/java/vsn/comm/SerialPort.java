4206890041626302c3d08c51823ce6f0436576d7
Jose Gisbert <jose.gisbert@live.com>
2015-12-30 17:02:18 +0100

    Documenta interfaz correspondiente a la conexión serie

--- src/main/java/vsn/comm/SerialPort.java
+++ src/main/java/vsn/comm/SerialPort.java
@@ -2,12 +2,32 @@ package vsn.comm;
 
 import javax.comm.CommPortIdentifier;
 
+/** Puerto serie para la comunicación con la matriz.
+  */
 public interface SerialPort {
 
+  /** Inicia la sesión con la matriz.
+    * @param port descriptor del puerto serie en el que se indican sus
+    *             características
+    * @return resultado del establecimiento de conexión especificado en el
+    *         manual de usuario
+    */
   int init(CommPortIdentifier port);
 
+  /** Envía un comando a la matríz.
+    * @param buffer secuencia de Bytes que representa el comando, corresponde a
+    *               aplicar el código especificado en la sección x.x del manual
+    *               del manual de usuario
+    * @param offset ...
+    * @param length ...
+    * @return número de Bytes enviados
+    */
   int sendCommand(byte[] buffer, int offset, int length);
 
+  /** Obtiene de la matriz la respuesta al último comando enviado.
+    * @param buffer ...
+    * @return número de Bytes recibidos
+    */
   int getResponse(byte[] buffer);
 
 }
